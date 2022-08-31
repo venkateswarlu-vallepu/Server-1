6f8ff3f313f2660af3ff3d19e159e2f58fe9b697
venkateswarlu-vallepu <108612511+venkateswarlu-vallepu@users.noreply.github.com>
2022-08-31 13:17:18 +0530

    Done as per the requirement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useRef} from 'react';
 
 function App() {
+   let nameDisplay = useRef();
+   let nameInput = useRef();
+   let emailInput = useRef();
+   let emailDisplay = useRef();
+   let idInput = useRef();
+   let idDisplay = useRef();
+  //  let name2 = useRef();
+  //  let email2 = useRef();
+  //  let name3 = useRef();
+  //  let email3 = useRef();
+  
+   
+  let onGetData = async ()=>{
+   
+    let reqOptions = {
+      method:"GET",
+
+    };
+    let rawData = await fetch("https://reqres.in/api/users?page=2", reqOptions);
+    
+    let convertedData = await rawData.json();
+
+    console.log("Getting Data From Server");
+    console.log(convertedData);
+  };
+   let gettingName = async ()=>{
+    
+    let requestMethods = {
+     method: "GET",
+    };
+    let rawData = await fetch("https://reqres.in/api/users?page=2",requestMethods);
+
+    let convertedData =  await rawData.json();
+   
+    let nameData = Number(nameInput.current.value)
+
+    nameDisplay.current.innerHTML = convertedData.data[nameData].first_name;
+   
+   };
+
+   let gettingEmail = async ()=>{
+    
+    let requestMethods = {
+     method:"GET",
+    };
+    let rawData = await fetch("https://reqres.in/api/users?page=2",requestMethods);
+
+    let convertedData =  await rawData.json();
+   
+    let emailData = Number(emailInput.current.value);
+
+    emailDisplay.current.innerHTML = convertedData.data[emailData].email;
+   
+   };
+
+   let gettingId = async ()=>{
+    let requestMethods = {
+      method: "GET",
+    };
+    let rawData = await fetch("https://reqres.in/api/users?page=2",requestMethods);
+
+    let convertedData =  await rawData.json();
+
+    let idData = Number(idInput.current.value);
+
+    idDisplay.current.innerHTML = convertedData.data[idData].id;
+  
+   } ;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Getting Data from Server-1</h1>
+    
+      <button type='button' onClick={()=>{
+        onGetData();
+      }}>Server Data</button>
+       <br></br>
+         <div className='div'>
+          <div className='idclass'>
+          <span>
+            <label className='label'>Check ID:</label>
+          </span>
+       <input id="login" type="number" min={0} max={5} ref={idInput} />
+          <label  ref={idDisplay}></label>
+
+      <button type='button' onClick={()=>{
+        gettingId();
+      }}>GetData</button>
+      </div>
+      <div className='idclass'>
+      <span>
+            <label className='label'>Check Name:</label>
+          </span>
+         <input id="login" type="number" min={0} max={5} ref={nameInput} />
+          <label  ref={nameDisplay}></label>
+
+      <button type='button' onClick={()=>{
+        gettingName();
+      }}>GetData</button>
+</div>
+      <div className='idclass'>
+      <span>
+            <label className='label'>Check Email:</label>
+          </span>
+      <input id="login" type="number" min={0} max={5} ref={emailInput} />
+          <label  ref={emailDisplay}></label>
+
+      <button type='button' onClick={()=>{
+       gettingEmail();
+      }}>GetData</button>
+      </div>
+</div>
     </div>
   );
 }
